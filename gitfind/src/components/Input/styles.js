ede64de6d991ba2dd5171b26c68eaee8e57a2f29
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-09-10 10:46:05 -0300

    commit

---
+++ gitfind/src/components/Input/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+
+    border: 1px solid #FAFAFA;
+    border-radius: 20px;
+
+    height: 62px;
+    width: 80%;
+
+    input {
+        background: transparent;
+        border: 0;
+    }
+`
+
---
+++ gitfind/src/pages/App.js
@@ -0,0 +1,19 @@
+import github from '../assets/github.png';
+import {Container} from './styles';
+import Input from '../components/Input';
+
+
+function App() {
+  return (   
+
+    
+    <Container>
+        <img src= {github} width={72} height={72} alt="Github-logo"/>
+        <input></input>      
+        
+      </Container> 
+  
+  );
+}
+
+export default App;
